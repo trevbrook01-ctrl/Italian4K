3f8f870ac6ad4951b58d24471beb33e3ac7372c1
Warren Peters <peters456@btinternet.com>
2026-02-17 18:45:06 +0000

    Initial Commit

---
+++ Italian4K/Italian4KApp.swift
@@ -0,0 +1,17 @@
+//
+//  Italian4KApp.swift
+//  Italian4K
+//
+//  Created by WARREN PETERS on 17/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Italian4KApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
